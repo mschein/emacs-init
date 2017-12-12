be9b7507f218fada6b76a2fd788896827d8666df
Michael Scheinholtz <mls@climate.com>
2017-12-11 22:05:03 -0600

    Add support for editing and viewing markdown files.

--- emacs-init.el
+++ emacs-init.el
@@ -934,7 +934,7 @@ that uses 'font-lock-warning-face'."
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (vkill dash-functional desktop+ dired-du dired-k dired-sort ivy paredit request restclient shell-command autopair macrostep dockerfile-mode async auto-complete clojure-mode dash epl f flycheck flycheck-perl6 flymake-go git-commit go-autocomplete go-guru go-mode go-playground go-snippets gotest json-mode let-alist magit-popup perl6-mode pkg-info popup queue seq spinner web-mode-edit-element which-key with-editor yasnippet magit google-this cider))))
+    (markdown-mode markdown-mode+ markdown-preview-mode macrostep dockerfile-mode async auto-complete clojure-mode dash epl f flycheck flycheck-perl6 flymake-go git-commit go-autocomplete go-guru go-mode go-playground go-snippets gotest json-mode let-alist magit-popup perl6-mode pkg-info popup queue s seq spinner web-mode web-mode-edit-element which-key with-editor yasnippet magit google-this cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ emacs-packages/markdown-mode-20171211.1056/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20171211.1056" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "b1fccf5cabd3e4f190647a4daa2d525e674e4dca" :url "https://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
