cc99a4037f16d1866cc7967bab99977e4cd5fb6a
mikes <mike@cronus.ws>
2018-09-01 17:46:36 -0500

    Add hyperbole mode and some other fun stuff.

---
+++ emacs-packages/python-info-20151228.1852/python-info-autoloads.el
@@ -0,0 +1,16 @@
+;;; python-info-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("python-info-pkg.el" "python-info.el")
+;;;;;;  (23435 5357 626868 339000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; python-info-autoloads.el ends here
