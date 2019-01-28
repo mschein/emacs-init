c808543c5e4315bda0dc0448164794f2a133fc86
Michael Scheinholtz <michael.scheinholtz@climate.com>
2019-01-28 16:20:18 -0600

    Make some improvements to the Jenkins code.

--- mine/jenkins.el
+++ mine/jenkins.el
@@ -32,38 +32,43 @@
 
 (defun jenkins-verify-password (jsi password)
   ;; Try to get the get the root info
-  (web-request-is-success (path-join (assoc1 :url jsi) "json")
+  (web-request-is-success (path-join (assoc1 :url jsi) "api" "json")
                           :auth (jenkins-make-auth jsi password)))
 
 (defun jenkins-get-password (jsi)
-  (read-user-password "Jenkins password: " "jenkins-password" #'jenkins-verify-password))
+  (read-user-password "Jenkins password: " "jenkins-password" (lambda (password) (jenkins-verify-password jsi password))))
 
 (defun jenkins-get-auth (jsi password-fn)
   (when-let (user (assoc-get :user jsi))
     (jenkins-make-auth jsi (jenkins-get-password jsi))))
 
-(defun jenkins-request (jsi path &rest args)
-  (assoc1 :json (apply #'web-request
-                       (path-join (assoc1 :url jsi) path "api" "json")
-                       :throw t
-                       :auth (jenkins-get-auth jsi #'jenkins-get-password)
-                       args)))
+(cl-defun jenkins-request (jsi path &key params)
+  (append-cons! params "pretty" "true")
+
+  (assoc1 :json (web-request
+                 (path-join (assoc1 :url jsi) path "api" "json")
+                 :throw t
+                 :auth (jenkins-get-auth jsi #'jenkins-get-password)
+                 :params params)))
 
 (defun jenkins-core-info (jsi)
-  (jenkins-request jsi "" :params '(("pretty" . "true"))))
+  (jenkins-request jsi ""))
 
 
 (defun jenkins-fetch-queue (jsi)
-  (jenkins-request jsi "queue" :params '(("pretty" . "true"))))
+  (jenkins-request jsi "queue"))
 
 (defun jenkins-overall-load (jsi)
-  (jenkins-request jsi "overallLoad" :params '(("pretty" . "true"))))
+  (jenkins-request jsi "overallLoad"))
 
 (defun jenkins-list-jobs (jsi)
-  (jenkins-request jsi "" :params '(("pretty" . "true")
-                                    ("tree" . "jobs[name,color]"))))
+  (jenkins-request jsi "" :params '(("tree" . "jobs[name,color]"))))
+
+(defun jenkins-list-nodes (jsi)
+  (jenkins-request jsi "computer")
+  )
 
 (defun jenkins-fetch-job (jsi job-path)
-  (jenkins-request jsi (path-join "job" job-path) :params '(("pretty" . "true"))))
+  (jenkins-request jsi (path-join "job" job-path)))
 
 (provide 'jenkins)
