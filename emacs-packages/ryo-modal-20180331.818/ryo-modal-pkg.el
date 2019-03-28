a1d9ac759e3241556082207ba64db110fcaf4a3a
mikes <mike@cronus.ws>
2019-03-28 10:39:45 -0500

    actually add ryo mode.

---
+++ emacs-packages/ryo-modal-20180331.818/ryo-modal-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ryo-modal" "20180331.818" "Roll your own modal mode" '((emacs "24.4")) :commit "42f874467dfdce59b511f883496ce2624b133dd7" :keywords '("convenience" "modal" "keys") :authors '(("Erik Sjöstrand" . "sjostrand.erik@gmail.com")) :maintainer '("Erik Sjöstrand" . "sjostrand.erik@gmail.com") :url "http://github.com/Kungsgeten/ryo-modal")
