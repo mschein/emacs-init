fd142011cbd79b0820f0bd9ffd9e466e3eba0f77
mikes <mike@cronus.ws>
2019-05-28 21:19:13 -0500

    add minutes to ffmpeg.

--- mine/ffmpeg.el
+++ mine/ffmpeg.el
@@ -25,10 +25,11 @@
 ;;  convert -loop 0 frames/ffout*.png output.gif
 
 ;; $ ffmpeg -ss 61.0 -t 2.5 -i StickAround.mp4 -filter_complex "[0:v] fps=12,scale=w=480:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" StickAroundPerFrame.gif
-(cl-defun ffmpeg-to-gif (input-file &key (scale-width 480) start length)
+(cl-defun ffmpeg-to-gif (input-file &key (scale-width 480) start minutes length)
   (assert (file-exists-p input-file))
 
-  (let ((output-file (concat (file-name-sans-extension input-file) ".gif")))
+  (let ((output-file (concat (file-name-sans-extension input-file) ".gif"))
+        (start (+ start (or (* 60 minutes) 0))))
     (do-cmd
      (list "ffmpeg"
            "-ss" (format "%s" start)
