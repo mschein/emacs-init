ac07cc93418044c282e5e0a5037ea5f30186509a
Michael Scheinholtz <michael.scheinholtz@climate.com>
2019-11-06 17:41:18 -0600

    Add new packages and attempt to get newer lsp-mode working.

---
+++ emacs-packages/lsp-java-20191030.1725/lsp-java-autoloads.el
@@ -0,0 +1,54 @@
+;;; lsp-java-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "lsp-java" "lsp-java.el" (24003 16760 174381
+;;;;;;  752000))
+;;; Generated autoloads from lsp-java.el
+(with-eval-after-load 'lsp-mode (require 'lsp-java))
+
+;;;***
+
+;;;### (autoloads nil "lsp-java-boot" "lsp-java-boot.el" (24003 16760
+;;;;;;  173521 900000))
+;;; Generated autoloads from lsp-java-boot.el
+
+(autoload 'lsp-java-boot-lens-mode "lsp-java-boot" "\
+Toggle code-lens overlays.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "lsp-jt" "lsp-jt.el" (24003 16760 171717 201000))
+;;; Generated autoloads from lsp-jt.el
+
+(autoload 'lsp-jt-lens-mode "lsp-jt" "\
+Toggle code-lens overlays.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'lsp-jt-browser "lsp-jt" "\
+
+
+\(fn)" t nil)
+
+(autoload 'lsp-jt-show-report "lsp-jt" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("lsp-java-pkg.el") (24003 16760 172526
+;;;;;;  421000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; lsp-java-autoloads.el ends here
