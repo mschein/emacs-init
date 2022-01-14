fb80a50b8091a10c903937a1a186ff40a77589de
mschein <mike@cronus.ws>
2022-01-13 19:13:41 -0600

    Add a function for chaining async calls to process a list.

--- mine/elisp-lib.el
+++ mine/elisp-lib.el
@@ -2593,6 +2593,24 @@ end tell
           old-env-values)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Async Utils
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun do-list-async (list fn cb)
+  "A helper/wrapper for when you want to do work on a list with a function that takes a callback.
+
+
+`list': a list of things that will get passed one at a time to the callback.
+
+"
+  (when list
+    (cl-labels ((handler-fn (&rest args)
+                  (apply cb args)
+                  (when list
+                    (funcall fn (pop list) #'handler-fn))))
+    (funcall fn (pop list) #'handler-fn))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Git commands
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
